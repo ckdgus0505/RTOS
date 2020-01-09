771e78eea19cd168f6ca2bb04e8ee6b258aa5b97
ckdgus0505 <ckdgus0505@gmail.com>
2020-01-10 03:25:05 +0900

    chapter 6

--- boot/Main.c
+++ boot/Main.c
@@ -1,6 +1,7 @@
 #include "stdint.h"
 #include "HalUart.h"
 #include "stdio.h"
+#include "stdbool.h"
 
 static void Hw_init(void);
 static void Print_test(void);
@@ -8,19 +9,23 @@ void main(void)
 {
 	Hw_init();
 
-	putstr("Hello World\n");
 	uint32_t i = 100;
-
-	Print_test();
 	while(i--)
 	{
-		uint8_t ch = Hal_uart_get_char();
-		Hal_uart_put_char(ch);
+		Hal_uart_put_char('N');
 	}
+	Hal_uart_put_char('\n');
+
+	putstr("Hello World!\n");
+	
+	Print_test();
+
+	while(true);
 }
 
 static void Hw_init(void)
 {
+	Hal_uart_init();
 	Hal_uart_init();
 }
 
@@ -29,12 +34,11 @@ static void Print_test(void)
 	char* str = "printf pointer test";
 	char* nullptr = 0;
 	uint32_t i = 5;
-
 	debug_printf("%s\n", "Hello printf");
 	debug_printf("output string pointer: %s\n", str);
 	debug_printf("%s is null pointer, %u number\n", nullptr, 10);
 	debug_printf("%u = 5\n", i);
-	debug_printf("dec=%u hex=%x%x\n", 1234567890, 0x12345678, 0x90ABCDEF);
-	debug_printf("printf zero %u\n", 0);
+	debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+	debug_printf("print zero %u\n", 0);
 }
 
