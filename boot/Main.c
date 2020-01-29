6e91847eaad2bf33bc45a519d6c795767eff27e0
ckdgus0505 <ckdgus0505@gmail.com>
2020-01-29 19:05:11 +0900

    chapter 12

--- boot/Main.c
+++ boot/Main.c
@@ -99,25 +99,37 @@ void User_task0(void)
 	uint32_t local = 0;
 	debug_printf("User Task #0 SP=0x%x\n", &local);
 
+	uint8_t cmdBuf[16];
+	uint32_t cmdBufIdx = 0;
+	uint8_t uartch = 0;
+
 	while(true)
 	{
-		bool pendingEvent = true;
-		while(pendingEvent)
+		KernelEventFlag_t handle_event = Kernel_wait_events(KernelEventFlag_UartIn|KernelEventFlag_CmdOut);
+		switch(handle_event)
 		{
-			KernelEventFlag_t handle_event = Kernel_wait_events(KernelEventFlag_UartIn|KernelEventFlag_CmdOut);
-			switch(handle_event)
+		case KernelEventFlag_UartIn:
+			Kernel_recv_msg(KernelMsgQ_Task0, &uartch, 1);
+			if (uartch == '\r')
 			{
-			case KernelEventFlag_UartIn:
-				debug_printf("\nEvent handled by Task0\n");
+				cmdBuf[cmdBufIdx] = '\0';
+
+				Kernel_send_msg(KernelMsgQ_Task1, &cmdBufIdx, 1);
+				Kernel_send_msg(KernelMsgQ_Task1, cmdBuf, cmdBufIdx);
 				Kernel_send_events(KernelEventFlag_CmdIn);
-				break;
-			case KernelEventFlag_CmdOut:
-				debug_printf("\nCmdOut Event by Task0\n");
-				break;
-			default:
-				pendingEvent = false;
-				break;
+
+				cmdBufIdx = 0;
 			}
+			else
+			{
+				cmdBuf[cmdBufIdx] = uartch;
+				cmdBufIdx++;
+				cmdBufIdx %= 16;
+			}
+			break;
+		case KernelEventFlag_CmdOut:
+			debug_printf("\nCmdOut Event by Task0\n");
+			break;
 		}
 		Kernel_yield();
 	}
@@ -126,13 +138,20 @@ void User_task1(void)
 {
 	uint32_t local = 0;
 	debug_printf("User Task #1 SP=0x%x\n", &local);
+
+	uint8_t cmdlen = 0;
+	uint8_t cmd[16] = {0};
+
 	while(true)
 	{
 		KernelEventFlag_t handle_event = Kernel_wait_events(KernelEventFlag_CmdIn);
 		switch(handle_event)
 		{
 		case KernelEventFlag_CmdIn:
-			debug_printf("\nEvent handled by Task1\n");
+			memclr(cmd, 16);
+			Kernel_recv_msg(KernelMsgQ_Task1, &cmdlen, 1);
+			Kernel_recv_msg(KernelMsgQ_Task1, cmd, cmdlen);
+			debug_printf("\nRecv Cmd: %s\n", cmd);
 			break;
 		}
 		Kernel_yield();
